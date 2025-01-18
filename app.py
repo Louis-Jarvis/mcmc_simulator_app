0a26dd7c6f8c788283e53601a666d2489c9fe5ec
Louis Jarvis <louisjarvisnz@gmail.com>
2025-01-18 23:06:52 +1300

    feat: use inbuild streamlit plots instead of matplotlib for performance

--- app.py
+++ app.py
@@ -1,107 +1,66 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 import logging
+import pandas as pd
 
 import pathlib
 
 from src.text import PROBLEM_DESCRIPTION, PROPOSAL_DISTRIBUTION_TEXT
 
-NUM_ITERATIONS = 100
+NUM_ITERATIONS = 1000
 
-logging.basicConfig(level=logging.INFO) #TODO change
+logging.basicConfig(level=logging.INFO)  # TODO change
 logger = logging.getLogger(__name__)
 
 sns.set_theme(style="darkgrid")
 
-#TODO eventually get rid of this.
+# Generate data
 x = np.linspace(0, 10, NUM_ITERATIONS)
 y = np.sin(x) + np.random.normal(0, 0.1, NUM_ITERATIONS)
 
-def update_trace_plot(x, y, idx, ax):
-    ax.set_xlim(0, 10)
-    ax.plot(x[:idx], y[:idx])
-    #sns.lineplot(x=x[:idx], y=y[:idx], ax=ax)  # Set x-limits
+# Initialize the trace plot with an empty DataFrame
+if "trace_data" not in st.session_state:
+    st.session_state.trace_data = pd.DataFrame(columns=['x', 'y'])
 
-def update_hist_plot(y, idx, ax):
-    #ax.clear()
-    ax.hist(y[:idx])
+if "hist_data" not in st.session_state:
+    st.session_state.hist_data = pd.DataFrame(columns=['Bins', 'Counts'])
 
-def update_all_plots(idx):
-    update_trace_plot(x, y, idx, st.session_state.axs[0])
-    for j in range(1, 4):
-        update_hist_plot(y, idx, st.session_state.axs[j])
+def update_trace_plot(x, y, idx):
+    # Append new data to the existing DataFrame
+    new_data = pd.DataFrame({'x': x[:idx], 'y': y[:idx]})
+    st.session_state.trace_data = pd.concat([st.session_state.trace_data, new_data], ignore_index=True)
 
-# initialisation
+def update_hist_plot(y, idx):
+    # Create a histogram and convert it to a DataFrame for bar chart
+    counts, bin_edges = np.histogram(y[:idx], bins=10)  # Adjust the number of bins as needed
+    hist_data = pd.DataFrame({'Bins': bin_edges[:-1], 'Counts': counts})
+    st.session_state.hist_data = hist_data
+
+# Initialization
 if "running" not in st.session_state:
     st.session_state.running = False
     st.session_state.idx = 0
 
-if "axs" not in st.session_state:
-    fig = plt.figure(figsize=(15, 10))
-    gs = fig.add_gridspec(2, 3, hspace=0.3, wspace=0.3)
-
-    # Top row:
-    ax1 = fig.add_subplot(gs[0, :])
-    ax1.set_title('Trace Plot')
-    ax1.set_xlim(0, NUM_ITERATIONS)
-
-    # Bottom row:
-    axs = [ax1]
-    for j in range(3):
-        ax = fig.add_subplot(gs[1, j])
-        axs.append(ax)
-
-    plt.suptitle('Metropolis Hastings Parameters', fontsize=16)
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust layout to make room for the title
-
-    st.session_state.plots = fig
-    st.session_state.axs = axs
-    
-
-#TODO add dependency injection
-def start_button():
-    st.session_state.running = True
-    logger.info("Starting")
-
-def stop_button():
-    st.session_state.running = False
-
-def reset_button():
-    st.session_state.running = False
-    st.session_state.idx = 0
-    [ax.clear() for ax in st.session_state.axs]
-
-
+# Sidebar controls
 st.title("Bayesian Linear Regression with MCMC")
-
-#############################################
-# Define the sidebar to control the animation
-#############################################
 st.sidebar.title("Inputs")
 
 with st.sidebar:
-
     main_button = st.empty()
     reset_button_ = st.empty()
 
     if not st.session_state.running:
         logger.debug("Starting")
-        main_button.button("Start", on_click=start_button)
+        main_button.button("Start", on_click=lambda: setattr(st.session_state, 'running', True))
     else:
-        main_button.button("Stop", on_click=stop_button)
+        main_button.button("Stop", on_click=lambda: setattr(st.session_state, 'running', False))
     
     if st.session_state.running or st.session_state.idx > 0:
         logger.debug("Reset button")
-        reset_button_.button("Reset", on_click=reset_button)
+        reset_button_.button("Reset", on_click=lambda: setattr(st.session_state, 'idx', 0))
 
-
-##############################################
-# Define the main content
-##############################################  
-
-#TODO tidy all this
+# Main content
 col1, col2 = st.columns(2)
 
 with col1:
@@ -112,27 +71,39 @@ with col2:
 
 st.markdown(PROPOSAL_DISTRIBUTION_TEXT)
 
-############################################
 # Run the simulation and display the plots
-############################################
 with st.container():
     trace_plot_a = st.empty()
 
+with st.container():
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        bar_chart_a = st.empty()
+    with col2:
+        bar_chart_b = st.empty()
+    with col3:
+        bar_chart_c = st.empty()
+
 with st.spinner("Running MCMC..."):
-
     for i in range(0, NUM_ITERATIONS, 10):
-
         if not st.session_state.running:
             break
 
-        update_trace_plot(x, y, i, st.session_state.axs[0])
+        logger.debug(f"Iteration {i}")
 
-        update_hist_plot(y, i, st.session_state.axs[1])
-        update_hist_plot(y, i, st.session_state.axs[2])
-        update_hist_plot(y, i, st.session_state.axs[3])
-        
-        plt.close()
-        trace_plot_a.pyplot(st.session_state.plots)
+        # Update the trace plot data
+        update_trace_plot(x, y, i)
+
+        # Use st.line_chart to display the trace plot
+        trace_plot_a.line_chart(st.session_state.trace_data.set_index('x'))
+
+        # Update histogram data
+        update_hist_plot(y, i)
+
+        # Use st.bar_chart to display the histogram as a bar chart
+        bar_chart_a.bar_chart(st.session_state.hist_data.set_index('Bins'))
+        bar_chart_b.bar_chart(st.session_state.hist_data.set_index('Bins'))
+        bar_chart_c.bar_chart(st.session_state.hist_data.set_index('Bins')) 
 
         st.session_state.idx += 1
 
