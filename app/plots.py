681468d50bfb73a88ea6c61e8a32e422ebb7feac
Louis Jarvis <louisjarvisnz@gmail.com>
2025-01-20 23:18:48 +1300

    refactor: refactored the histogram plotting function removed the need for manual histogram creation with pandas

--- app/plots.py
+++ app/plots.py
@@ -15,14 +15,14 @@ class SeabornPlotStyles:
     TITLE_COLOR = "#ECEFF4"
 
 
-def trace_plot(data):
+def trace_plot(data: pd.DataFrame) -> alt.Chart:
     """Create a trace plot of the parameters vs the number of iterations."""
     trace_chart = alt.Chart(data).mark_line(color='#e76f51').encode(
         x=alt.X('x', title='Number of Iterations'),
         y=alt.Y('y', title='Parameter Value')
     ).properties(title='Trace plot of parameters').configure_view(
         stroke='transparent',  # Remove the border around the chart
-        fill=SeabornPlotStyles.BACKGROUND_COLOR  # Dark Slate Gray background
+        fill=SeabornPlotStyles.BACKGROUND_COLOR
     ).configure_axis(
         grid=True,
         gridColor=SeabornPlotStyles.AXIS_COLOR, 
@@ -33,25 +33,15 @@ def trace_plot(data):
 
     return trace_chart
 
-def histogram_plot(data, variable, title, bins=10):
-    # Bin the data using pandas.cut
-    if data.empty:
-        # cannot cut pd.cut to empty data
-        histogram_data = pd.DataFrame(columns=['Bins', 'Counts'])
-    else:
-        histogram_data = pd.cut(data[variable], bins=bins, include_lowest=True).value_counts().sort_index().reset_index()
-        histogram_data.columns = [variable, 'Counts']  # Rename columns to match expected format
-        histogram_data['bin_centres'] = histogram_data[variable].apply(lambda x: (x.left + x.right) / 2)
+def histogram_plot(data: pd.DataFrame, variable: str, title: str, bins: int = 10) -> alt.Chart:
 
-
-    histogram = alt.Chart(histogram_data).mark_bar(opacity=0.8, color='#81A1C1', binSpacing=0).encode(
-        x=alt.X('bin_centres:Q', title=variable),  # Changed to ordinal type for binned data
-        y=alt.Y('Counts:Q', title=None)
-    ).properties(title=f"Distribution of {variable}").configure_view(
-        #stroke='transparent',  # Remove the border around the chart
+    histogram = alt.Chart(data).mark_bar(opacity=0.8, color='#81A1C1', binSpacing=0).encode(
+        x=alt.X('y:Q', title=variable).bin(maxbins=bins),  # Changed to ordinal type for binned data
+        y="count()"
+    ).properties(title=f"Distribution of {title}").configure_view(
         fill=SeabornPlotStyles.BACKGROUND_COLOR  # Background color
     ).configure_axis(
-        grid=True,  # Show grid lines
+        grid=True,
         gridColor=SeabornPlotStyles.AXIS_COLOR,  # Grid lines color
         tickColor=SeabornPlotStyles.AXIS_LABEL_COLOR,
         labelColor=SeabornPlotStyles.AXIS_LABEL_COLOR
