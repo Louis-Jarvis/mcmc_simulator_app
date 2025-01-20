3177006514631b10f1087947234657db983d7d8c
Louis Jarvis <louisjarvisnz@gmail.com>
2025-01-21 00:07:00 +1300

    feat: implement mcmc solver

---
+++ app/mcmc.py
@@ -0,0 +1,106 @@
+import numpy as np
+import scipy.stats as stats
+
+
+NUM_PARAMETERS = 3
+
+# this is an extra parameter that helps to adjust the spread of the distribution
+# see here https://medium.com/@tinonucera/bayesian-linear-regression-from-scratch-a-metropolis-hastings-implementation-63526857f191
+OMEGA = 500 
+
+
+def proposal_ratio(theta: np.ndarray, theta_prime: np.ndarray, k: float) -> np.ndarray:
+    """Likelihood of observing theta-prime given theta"""
+
+    # what is the likelihood of observing the old parameters given the new parameters
+    # this is the markov chain transition probability q(theta_prime|theta)
+    q_theta_theta_prime = stats.multivariate_normal.logpdf(theta_prime, mean=theta[0], cov=np.eye(2)*k**2)
+
+    # what is the likelihood of observing the new parameters given the old parameters
+    # this is the markov chain transition probability q(theta|theta_prime)
+    q_theta_prime_theta = stats.multivariate_normal.logpdf(theta, mean=theta_prime[0], cov=np.eye(2)*k**2)
+
+    # the ratio of the two probabilities is the proposal ratio
+    # we are adding them since we are working with log likelihoods
+
+    return q_theta_theta_prime - q_theta_prime_theta
+
+
+def propose_theta(theta: np.ndarray, k: float) -> np.ndarray:
+    """
+    Propose a new parameter value, theta_prime, given the current parameter vector theta.
+    where theta=[a,b,sigma]
+    """
+
+    theta_prime = np.zeros(3)
+
+    previous_a_b = theta[0:2]
+    previous_sigma = theta[2]
+    
+    # a and b - proposed based on the previous value of theta
+    theta_prime[0:2] = stats.multivariate_normal(mean=previous_a_b, cov=np.eye(2)*k**2).rvs(1)
+    
+    # sigma
+    theta_prime[2] = stats.gamma(a=previous_sigma*k*OMEGA, scale=k*(1/OMEGA)).rvs(1)
+    
+    return theta_prime
+
+def log_likelihood(data, theta):
+    """ 
+    Evaluate the likelhood of the data given a specific paramaeter vector.
+    We are assuming the conditional distribution of the response Y|X is normal
+    with mean a + b*X and variance sigma^2.
+    """
+
+    a,b,sigma = theta
+    # get the mean and variance of the conditional distribution
+    mean = a + b*data
+    variance = sigma**2
+
+    # we use the log-likeihood so that we are summing instead of multiplying
+    # this is to avoid numerical issues with small numbers
+    log_lik = stats.norm.logpdf(data, loc=mean, scale=variance)
+    return np.sum(log_lik)
+
+def log_prior(theta):
+    """ 
+    Evaluate the log-prior of the parameter vector theta.
+    """
+    # Similar to the proposal distriibution we will assume a normal prior for a and b
+    # as well as a gamma prior for sigma
+
+    a,b,sigma = theta
+    # assuming independence we can sum these together
+    log_prior = stats.norm.logpdf(a, loc=0, scale=1)
+    log_prior += stats.norm.logpdf(b, loc=0, scale=1) 
+    log_prior += stats.gamma.logpdf(sigma, a=1, scale=1)
+
+    return log_prior
+
+
+def log_posterior(data, theta):
+    return log_likelihood(data, theta) + log_prior(theta)
+
+
+def metropolis_hastings(data, theta, k, num_iterations):
+    
+    thetas = np.zeros((num_iterations, 3))
+    thetas[0,:] = np.random.rand(3)
+
+    for i in range(num_iterations):
+        
+        theta_prime = propose_theta(thetas[-1,:], k)
+
+        log_posterior_theta_prime = log_posterior(data, theta_prime)
+        log_posterior_theta = log_posterior(data, thetas[-1,:])
+        
+        # instead of dividing we are subtracting the log values
+        acceptance_ratio = log_posterior_theta_prime - log_posterior_theta
+        
+        # exponentiate the log acceptance ratio to obtain probability
+        if np.random.rand() < np.exp(acceptance_ratio):
+            thetas[i,:] = theta_prime
+        else:
+            thetas[i,:] = thetas[-1,:]
+
+    return thetas
